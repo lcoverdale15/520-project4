7866cc27a92b9e02b53cadacae4650d6e8c37fdf
lcoverdale15 <154622292+lcoverdale15@users.noreply.github.com>
2025-04-28 11:06:38 -0500

    Update MPI.sh

--- 3way-MPI/MPI.sh
+++ 3way-MPI/MPI.sh
@@ -5,9 +5,6 @@
 #SBATCH --mem-per-cpu=2G  # Increase memory per CPU to 2 because 1 is not enough
 #SBATCH --time=01:00:00  # Set a time limit just incase something breaks
 #SBATCH --constraint=moles
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
-
 
 
 module load OpenMPI/4.1.4-GCC-11.3.0
